682b5317c9b142a4dd015b9c4422c15430194857
alexhroom <alex.room@btinternet.com>
2023-03-09 13:52:02 +0000

    ran hindent

--- app/Main.hs
+++ app/Main.hs
@@ -1,81 +1,99 @@
-module Main (main) where
+module Main
+  ( main
+  ) where
 
-import Control.Monad(forM)
-import System.IO
-import System.FilePath ( takeBaseName, normalise, (</>), (-<.>), takeDirectory)
-import System.Directory (doesDirectoryExist, getDirectoryContents, createDirectoryIfMissing)
-import System.Environment(getArgs)
-import Data.Maybe(fromMaybe)
+import Control.Monad (forM)
+import Data.Maybe (fromMaybe)
 import Data.Text (unpack)
 import Options
+import System.Directory
+  ( createDirectoryIfMissing
+  , doesDirectoryExist
+  , getDirectoryContents
+  )
+import System.Environment (getArgs)
+import System.FilePath ((-<.>), (</>), normalise, takeBaseName, takeDirectory)
+import System.IO
 
-import Parse(parseToBlocks)
 import Blocks
-import Header(decodeHeader, HeaderTOML(tomlHeaderBeginSrc, tomlHeaderEndSrc, tomlHeaderWeave, tomlHeaderTangle))
+import Header
+  ( HeaderTOML(tomlHeaderBeginSrc, tomlHeaderEndSrc, tomlHeaderTangle,
+           tomlHeaderWeave)
+  , decodeHeader
+  )
+import Parse (parseToBlocks)
 
-data MainOptions = MainOptions
+data MainOptions =
+  MainOptions
     { optWeaveDir :: String
     , optTangleDir :: String
-    } deriving Show
+    }
+  deriving (Show)
 
 {-# ANN module "HLint: ignore Use <$>" #-}
+
 instance Options MainOptions where
-    defineOptions = pure MainOptions
-        <*> defineOption optionType_string (\o -> o
-        {
-          optionLongFlags = ["weavedir"]
-        , optionShortFlags = ['w']
-        , optionDefault = ""
-        , optionDescription = "The directory to weave to. Defaults to working directory."
-        })
-        <*> defineOption optionType_string (\o -> o
-        {
-          optionLongFlags = ["tangledir"]
-        , optionShortFlags = ['t']
-        , optionDefault = ""
-        , optionDescription = "The directory to tangle to. Defaults to working directory."
-        })
+  defineOptions =
+    pure MainOptions <*>
+    defineOption
+      optionType_string
+      (\o ->
+         o
+           { optionLongFlags = ["weavedir"]
+           , optionShortFlags = ['w']
+           , optionDefault = ""
+           , optionDescription =
+               "The directory to weave to. Defaults to working directory."
+           }) <*>
+    defineOption
+      optionType_string
+      (\o ->
+         o
+           { optionLongFlags = ["tangledir"]
+           , optionShortFlags = ['t']
+           , optionDefault = ""
+           , optionDescription =
+               "The directory to tangle to. Defaults to working directory."
+           })
 
 main :: IO [()]
 main = do
-         argv <- getArgs
-         let parsed = parseOptions argv :: ParsedOptions MainOptions
-         let opts = fromMaybe MainOptions {optWeaveDir = "", optTangleDir = ""} (parsedOptions parsed)
-         paths <- getRecursiveContents $ head $ parsedArguments parsed
-
-         mapM (processFile (optWeaveDir opts) (optTangleDir opts)) paths
+  argv <- getArgs
+  let parsed = parseOptions argv :: ParsedOptions MainOptions
+  let opts =
+        fromMaybe
+          MainOptions {optWeaveDir = "", optTangleDir = ""}
+          (parsedOptions parsed)
+  paths <- getRecursiveContents $ head $ parsedArguments parsed
+  mapM (processFile (optWeaveDir opts) (optTangleDir opts)) paths
 
 processFile :: String -> String -> FilePath -> IO ()
 processFile weavedir tangledir file = do
-        inputfile <- openFile file ReadMode
-        contents <- hGetContents inputfile
-        let blocks = concatBlocks . parseToBlocks $ contents
-        print file
-        if not (any isHeader blocks)
-                then return ()
-                else do
-                let header = decodeHeader . blockContents . getHeader $ blocks
+  inputfile <- openFile file ReadMode
+  contents <- hGetContents inputfile
+  let blocks = concatBlocks . parseToBlocks $ contents
+  print file
+  if not (any isHeader blocks)
+    then return ()
+    else do
+      let header = decodeHeader . blockContents . getHeader $ blocks
                 -- get relevant strings from header
-                let tanglefile = tangledir </>
-                                 file -<.> unpack (tomlHeaderTangle header)
-                let weavefile = weavedir </>
-                                file -<.> unpack (tomlHeaderWeave header)
-                let bsrc = unpack $ tomlHeaderBeginSrc header
-                let esrc = unpack $ tomlHeaderEndSrc header
+      let tanglefile = tangledir </> file -<.> unpack (tomlHeaderTangle header)
+      let weavefile = weavedir </> file -<.> unpack (tomlHeaderWeave header)
+      let bsrc = unpack $ tomlHeaderBeginSrc header
+      let esrc = unpack $ tomlHeaderEndSrc header
+      _ <- createDirectoryIfMissing True (takeDirectory tanglefile)
+      _ <- createDirectoryIfMissing True (takeDirectory weavefile)
+      tangle blocks tanglefile
+      weave blocks weavefile bsrc esrc
 
-                _ <- createDirectoryIfMissing True (takeDirectory tanglefile)
-                _ <- createDirectoryIfMissing True (takeDirectory weavefile)
-                tangle blocks tanglefile
-                weave blocks weavefile bsrc esrc
-
-tangle :: [Block] -> String -> IO()
+tangle :: [Block] -> String -> IO ()
 tangle = flip writeFile . blocksToString . tangleFilter
 
-weave :: [Block] -> String -> String -> String -> IO()
-weave blocks filename bsrc esrc = writeFile filename
-                                  $ concatMap (blockContents
-                                  . padCode bsrc esrc)
-                                  $ weaveFilter blocks
+weave :: [Block] -> String -> String -> String -> IO ()
+weave blocks filename bsrc esrc =
+  writeFile filename $
+  concatMap (blockContents . padCode bsrc esrc) $ weaveFilter blocks
 
 getHeader :: [Block] -> Block
 getHeader = head . filter isHeader
@@ -89,15 +107,16 @@ tangleFilter = filter isCode
 
 weaveFilter :: [Block] -> [Block]
 weaveFilter = filter notHiddenOrHeader
-        where notHiddenOrHeader :: Block -> Bool
-              notHiddenOrHeader (Block t _)
-                                | t == HiddenCode || t == Header = False
-                                | otherwise = True
+  where
+    notHiddenOrHeader :: Block -> Bool
+    notHiddenOrHeader (Block t _)
+      | t == HiddenCode || t == Header = False
+      | otherwise = True
 
 padCode :: String -> String -> Block -> Block
-padCode begin end block@(Block t txt) 
-        | t == Code || t == MultiCode = Block t (concat [begin, "\n", txt, "\n", end])
-        | otherwise = block
+padCode begin end block@(Block t txt)
+  | t == Code || t == MultiCode = Block t (concat [begin, "\n", txt, "\n", end])
+  | otherwise = block
 
 blocksToString :: [Block] -> String
 blocksToString = concatMap ((++ "\n\n") . blockContents)
@@ -107,10 +126,11 @@ getRecursiveContents :: FilePath -> IO [FilePath]
 getRecursiveContents topdir = do
   names <- getDirectoryContents topdir
   let properNames = filter (`notElem` [".", ".."]) names
-  paths <- forM properNames $ \name -> do
-    let path = topdir </> name
-    isDirectory <- doesDirectoryExist path
-    if isDirectory
-      then getRecursiveContents path
-      else return [normalise path]
+  paths <-
+    forM properNames $ \name -> do
+      let path = topdir </> name
+      isDirectory <- doesDirectoryExist path
+      if isDirectory
+        then getRecursiveContents path
+        else return [normalise path]
   return (concat paths)
--- src/Blocks.hs
+++ src/Blocks.hs
@@ -1,10 +1,10 @@
-module Blocks 
-       ( BlockType (Text, Code, HiddenCode, MultiCode, Header, Error, Empty)
-       , Block (Block)
-       , concatBlocks
-       , blockContents
-       , isCode
-       ) where
+module Blocks
+  ( BlockType(Text, Code, HiddenCode, MultiCode, Header, Error, Empty)
+  , Block(Block)
+  , concatBlocks
+  , blockContents
+  , isCode
+  ) where
 
 {-
 | A datatype which annotates the type of block:
@@ -16,47 +16,51 @@ module Blocks
 - Error: An error type to indicate a parser failure.
 - Empty: A non-error empty block, to capture parser edge cases.
 -}
-data BlockType = Text
-               | Code
-               | HiddenCode
-               | MultiCode
-               | Header
-               | Error
-               | Empty
-               deriving (Show, Eq)
+data BlockType
+  = Text
+  | Code
+  | HiddenCode
+  | MultiCode
+  | Header
+  | Error
+  | Empty
+  deriving (Show, Eq)
 
-{- 
+{-
 | Block annotates a string with a BlockType, documented above.
 It is used to format a FREEBIRD file into woven or tangled format.
 -}
-data Block = Block BlockType String deriving Show
+data Block =
+  Block BlockType String
+  deriving (Show)
 
 -- predicate for whether a block is of 'code' type
 isCode :: Block -> Bool
 isCode (Block t _)
-        | t == Code || t == HiddenCode || t == MultiCode = True
-        | otherwise = False
+  | t == Code || t == HiddenCode || t == MultiCode = True
+  | otherwise = False
 
 -- function which checks two blocks are both the same block type
 sameType :: Block -> Block -> Bool
 sameType (Block t1 _) (Block t2 _)
-        | t1 == t2 = True
-        | otherwise = False
+  | t1 == t2 = True
+  | otherwise = False
 
 -- function which concatenates two blocks; used to combine code lines into code blocks.
 concatBlock :: Block -> Block -> Block
-concatBlock (Block type1 val1) (Block _ val2) = Block type1 (val1 ++ "\n" ++ val2)
+concatBlock (Block type1 val1) (Block _ val2) =
+  Block type1 (val1 ++ "\n" ++ val2)
 
 -- map of concatBlock which concatenates adjacent Code blocks in a document.
 concatBlocks :: [Block] -> [Block]
 concatBlocks [] = []
 concatBlocks [x] = [x]
 concatBlocks [x, y]
-    | isCode x && isCode y && sameType x y = [concatBlock x y]
-    | otherwise = [x, y]
+  | isCode x && isCode y && sameType x y = [concatBlock x y]
+  | otherwise = [x, y]
 concatBlocks (x:y:zs)
-    | isCode x && isCode y && sameType x y = concatBlocks (concatBlock x y : zs)
-    | otherwise = x : concatBlocks (y:zs)
+  | isCode x && isCode y && sameType x y = concatBlocks (concatBlock x y : zs)
+  | otherwise = x : concatBlocks (y : zs)
 
 -- accessor function to return the contents of a block.
 blockContents :: Block -> String
