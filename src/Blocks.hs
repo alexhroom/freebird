d582c34737eaa65335001bb8e7a838c32cdd7ab3
alexhroom <alex.room@btinternet.com>
2023-03-09 12:35:18 +0000

    fixed some edge cases in block concatenation

--- src/Blocks.hs
+++ src/Blocks.hs
@@ -37,6 +37,12 @@ isCode (Block t _)
         | t == Code || t == HiddenCode || t == MultiCode = True
         | otherwise = False
 
+-- function which checks two blocks are both the same block type
+sameType :: Block -> Block -> Bool
+sameType (Block t1 _) (Block t2 _)
+        | t1 == t2 = True
+        | otherwise = False
+
 -- function which concatenates two blocks; used to combine code lines into code blocks.
 concatBlock :: Block -> Block -> Block
 concatBlock (Block type1 val1) (Block _ val2) = Block type1 (val1 ++ "\n" ++ val2)
@@ -45,8 +51,11 @@ concatBlock (Block type1 val1) (Block _ val2) = Block type1 (val1 ++ "\n" ++ val
 concatBlocks :: [Block] -> [Block]
 concatBlocks [] = []
 concatBlocks [x] = [x]
+concatBlocks [x, y]
+    | isCode x && isCode y && sameType x y = [concatBlock x y]
+    | otherwise = [x, y]
 concatBlocks (x:y:zs)
-    | isCode x && isCode y = concatBlocks (concatBlock x y : zs)
+    | isCode x && isCode y && sameType x y = concatBlocks (concatBlock x y : zs)
     | otherwise = x : concatBlocks (y:zs)
 
 -- accessor function to return the contents of a block.
